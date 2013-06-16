0bed8288616d0f26946a3db9d8c6a704806b755d
Ask Bjørn Hansen <ask@develooper.com>
2013-06-16 15:17:07 -0700

    Add DayDuration type

---
+++ dayduration_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"time"
+)
+
+type DurationSuite struct {
+}
+
+var _ = Suite(&DurationSuite{})
+
+func (s *DurationSuite) TestDayDuration(c *C) {
+
+	d := DayDuration{time.Second * 5}
+	c.Check(d.DayString(), Equals, "5s")
+
+	d = DayDuration{time.Second * 3670}
+	c.Check(d.DayString(), Equals, "1h 1m")
+
+}
